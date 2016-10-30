732bab0bf6bfbacf1b67c567b4600bf982d881be
James Punnett <jppunnett@yahoo.com>
2016-10-30 13:34:55 -0400

    Initial commit.

---
+++ play.c
@@ -0,0 +1,15 @@
+/*
+    Play something on my Jam Box.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+
+int main()
+{
+    exit(EXIT_SUCCESS);
+}
