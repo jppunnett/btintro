09d5c6bb41f655b3a790a186c77a21d8c1710040
James Punnett <jppunnett@yahoo.com>
2016-10-30 11:17:25 -0400

    Check if Bluetooth header and library exists

---
+++ scan.c
@@ -0,0 +1,15 @@
+/*
+	Scan for nearby bluetooth devices.
+
+	Skeleton based on: http://people.csail.mit.edu/albert/bluez-intro/c404.html
+	Compile using: gcc -o scan.out scan.c -lbluetooth
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <bluetooth/bluetooth.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello Bluetooth!\n");
+	exit(EXIT_SUCCESS);
+}
